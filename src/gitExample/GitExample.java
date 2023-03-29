b952939b37c8a1e9c8bf31061f510b9074b5888d
Vindhuja2023 <vindhuja.rajeev@gmail.com>
2023-03-29 16:25:47 -0400

    adding new files

---
+++ src/gitExample/GitExample.java
@@ -0,0 +1,12 @@
+package gitExample;
+
+public class GitExample {
+	
+	void exampleGit() {
+		
+	System.out.println("Git example" );
+	
+	System.out.println("testing" );
+	
+	}
+}
---
+++ src/gitExample/mainClass.java
@@ -0,0 +1,11 @@
+package gitExample;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         GitExample ex = new  GitExample();
+         ex.exampleGit();
+	}
+
+}
