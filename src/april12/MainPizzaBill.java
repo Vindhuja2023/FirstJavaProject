fcd5ffa5e7d38f3f2ee0808cc6b370de5bbaf1a4
Vindhuja2023 <vindhuja.rajeev@gmail.com>
2023-04-13 12:54:12 -0400

    Assignment

---
+++ src/april12/MainPizzaBill.java
@@ -0,0 +1,16 @@
+package april12;
+
+
+
+public class MainPizzaBill {
+
+	public static void main(String[] args) {
+		PizzaBill pizza = new PizzaBill();
+		String[] pizzaSize = {"Small","Medium","Large"};
+		pizza.displayingPizzaDetails(pizzaSize);
+		pizza.bill(pizzaSize);
+
+
+	}
+
+}
